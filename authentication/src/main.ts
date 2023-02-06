53cd3b3300c6feb8e2f25417420a416ef82a0153
Max <mdraginich@gmail.com>
2023-02-06 14:13:13 +0200

    upd

--- authentication/src/main.ts
+++ authentication/src/main.ts
@@ -4,21 +4,14 @@ import { AuthModule } from './auth.module';
 
 import { ConfigService } from './services/config/config.service';
 
-// async function bootstrap() {
-// 	const app = await NestFactory.createMicroservice<MicroserviceOptions>(AuthModule, {
-// 		transport: Transport.REDIS,
-// 		options: {
-// 			host: new ConfigService().get('HOST'),
-// 			port: new ConfigService().get('PORT'),
-// 		},
-// 	} as RedisOptions);
-// 	await app.listen();
-// }
-// bootstrap();
-
-
 async function bootstrap() {
-	const app = await NestFactory.create(AuthModule);
-	await app.listen(3000);
+	const app = await NestFactory.createMicroservice<MicroserviceOptions>(AuthModule, {
+		transport: Transport.REDIS,
+		options: {
+			host: new ConfigService().get('HOST'),
+			port: new ConfigService().get('PORT'),
+		},
+	} as RedisOptions);
+	await app.listen();
 }
 bootstrap();
